2353f92eaaba050dda305229a033562a3e0fdb3b
sinjouji <mail0of0iris@gmail.com>
2026-05-02 00:04:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 // ★ あなたのJSON URL
 const DATA_URL = "https://raw.githubusercontent.com/sinjouji/my-b0o0oksd6t6/main/data.json";
 
-
-
 let books = [];
+
+
+
+
+
+
+
+
+function go(name){
+  document.querySelectorAll('.page').forEach(p=>{
+    p.classList.add('hidden');
+  });
+
+typeof go
+
+  document.getElementById('page-' + name).classList.remove('hidden');
+}
+
+
+//ホーム表示
+function renderHome(){
+  const el = document.getElementById('page-home');
+  el.innerHTML = "";
+
+  books.forEach(b=>{
+    const d = document.createElement('div');
+    d.className = "card";
+    d.textContent = b.title;
+
+    d.onclick = ()=> openDetail(b);
+
+    el.appendChild(d);
+  });
+}
+
+//詳細ページ
+function openDetail(book){
+  go('detail');
+
+  const el = document.getElementById('page-detail');
+
+  el.innerHTML = `
+    <h2>${book.title}</h2>
+    <div>お気に入り: ${book.fav}</div>
+    <div>${book.memo || ""}</div>
+    <button onclick="go('home')">戻る</button>
+  `;
+}
+
 
 async function loadData(){
 
@@ -58,46 +105,6 @@ function render(){
   });
 
 }
-
-function go(name){
-  document.querySelectorAll('.page').forEach(p=>{
-    p.classList.add('hidden');
-  });
-
-  document.getElementById('page-' + name).classList.remove('hidden');
-}
-
-
-//ホーム表示
-function renderHome(){
-  const el = document.getElementById('page-home');
-  el.innerHTML = "";
-
-  books.forEach(b=>{
-    const d = document.createElement('div');
-    d.className = "card";
-    d.textContent = b.title;
-
-    d.onclick = ()=> openDetail(b);
-
-    el.appendChild(d);
-  });
-}
-
-//詳細ページ
-function openDetail(book){
-  go('detail');
-
-  const el = document.getElementById('page-detail');
-
-  el.innerHTML = `
-    <h2>${book.title}</h2>
-    <div>お気に入り: ${book.fav}</div>
-    <div>${book.memo || ""}</div>
-    <button onclick="go('home')">戻る</button>
-  `;
-}
-
 
 // 初回ロード
 
