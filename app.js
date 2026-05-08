13f6048753f4f7dd7f3f3be9cda63f9a63f46427
sinjouji <mail0of0iris@gmail.com>
2026-05-08 18:44:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -935,9 +935,9 @@ function renderShelfView(main, books){
         ${b.title}
       </div>
       
-  //    <div class="book-read-count">
-  //    ${book.dates?.length || 0}回
-  //    </div>
+      <!--<div class="book-read-count">-->
+      <!--${book.dates?.length || 0}回-->
+      <!--</div>-->
 
       <div class="spine-fav">
         ${getFavLabel(b.fav)}
