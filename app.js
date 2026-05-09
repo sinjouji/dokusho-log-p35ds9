f78f9eab34eaf67136d4cda89b3ad048523fd2fb
sinjouji <mail0of0iris@gmail.com>
2026-05-09 14:12:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ function go(page){
   if(page === 'series') renderSeries();
   if(page === 'characters') renderCharacters();
   if(page === 'calendar') renderCalendar();
+  if(page === 'stats') renderStats();
 }
 //========
 
