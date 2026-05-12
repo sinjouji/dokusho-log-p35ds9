7f41f6e782e9e57898e0ccfd48c8b98e39d483a3
sinjouji <mail0of0iris@gmail.com>
2026-05-13 08:15:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2037,7 +2037,7 @@ function renderMiniCalendar(main){
 
     if(dateStr === today){
       cell.style.border =
-        "2px solid #954039";
+        "2px solid #e8380d;
     }
 
     cell.innerHTML =
