40b796d5aa3e705f377e9472d2e0a570abf9ad1c
sinjouji <mail0of0iris@gmail.com>
2026-05-13 02:38:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1396,7 +1396,7 @@ function renderCardView(main, books){
 
     const d = document.createElement("div");
 
-    d.className = "card honlist";
+    d.className = "honlist";
 
     d.innerHTML = `
       <div class="title">${b.title}</div>
