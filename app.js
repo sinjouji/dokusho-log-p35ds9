434556d82bbf093078d94b274d1ae9f83c7ff7da
sinjouji <mail0of0iris@gmail.com>
2026-05-13 21:10:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -960,9 +960,9 @@ function openBookDetailModal(book){
 
   currentDetailFav = book.fav || 0;
   
-  const relatedSeries = seriesMaster.filter(s=>{
-  return Array.isArray(s.bookIds) && s.bookIds.includes(book.id);
-  });
+ // const relatedSeries = seriesMaster.filter(s=>{
+ // return Array.isArray(s.bookIds) && s.bookIds.includes(book.id);
+ // });
   
   const modal = document.createElement("div");
   modal.className = "modal-bg";
