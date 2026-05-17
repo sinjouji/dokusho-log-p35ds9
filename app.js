7c0c08f110ea11a9866bed43e8c743a6aba910e4
sinjouji <mail0of0iris@gmail.com>
2026-05-17 23:21:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1229,7 +1229,7 @@ function openBookDetailModal(book){
       
       
       <div class="detail-date-add">
-      <input type="date" id="readDate-${book.id}" style="padding:1px 2px;width:80%;">
+      <input type="date" id="readDate-${book.id}" style="padding:1px 2px;width:70%;">
       <button onclick="addReadDate('${book.id}')" style="margin-left:auto;">
         ➕読了日
       </button>
