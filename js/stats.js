08ceb241625c6a314b3078996c70acd24381709b
sinjouji <mail0of0iris@gmail.com>
2026-06-01 15:47:28 +0900

    Add files via upload

--- js/stats.js
+++ js/stats.js
@@ -389,7 +389,8 @@ function getHeatColor(count){
   if(count === 3) return "#c8ddb0"; //苗
   
   if(count === 4) return "#a2d7dd"; //瓶覗
-  return "#84c9d4"; //
+  return "#839cca";//青藤
+//  return "#84c9d4"; //gptちゃん提案色
   }
 
 
