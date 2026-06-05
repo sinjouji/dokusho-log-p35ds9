6ab60c916ad0b7b1f1134811b9e387587260816a
sinjouji <mail0of0iris@gmail.com>
2026-06-05 19:24:12 +0900

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -560,77 +560,72 @@ id="open-book-tags">
        ${
   hiddenTagIds.length
     ? `
-    
-    
+      <div class="hidden-tag-area">
+
         <div class="hidden-tag-title">
           管理タグ（${hiddenTagIds.length}）
         </div>
-      <div class="hidden-tag-area"></div>
-  <div class="hidden-tag-input-area">
-    <div class="hidden-tag-list selected-hidden-tags">
 
-      ${
-        (book.tagIds || [])
-          .filter(tagId =>
-            isHiddenTag(tagId)
-          )
-          .map(tagId=>{
+        <div class="hidden-tag-list selected-hidden-tags">
+          ${
+            hiddenTagIds.map(tagId=>{
 
-            const tag =
-              tagMaster.find(t =>
-                String(t.id) ===
-                String(tagId)
-              );
+              const tag =
+                tagMaster.find(t =>
+                  String(t.id) === String(tagId)
+                );
 
-            if(!tag) return "";
+              if(!tag) return "";
 
-            return `
-              <span
-  class="
-    hidden-tag-chip
-    ${
-      pendingHiddenTagRemove === `${book.id}-${tag.id}`
-        ? "remove-ready"
-        : ""
-    }
-  "
-  onclick="confirmRemoveHiddenTag('${book.id}','${tag.id}')"
->
-  ${
-    pendingHiddenTagRemove === `${book.id}-${tag.id}`
-      ? `削除？ ${tag.name}`
-      : tag.name
-  }
-</span>
-            `;
-          })
-          .join("")
-      }
+              const isRemoveReady =
+                pendingHiddenTagRemove ===
+                `${book.id}-${tag.id}`;
 
-    </div>
+              return `
+                <span
+                  class="
+                    hidden-tag-chip
+                    ${isRemoveReady ? "remove-ready" : ""}
+                  "
+                  onclick="
+                    confirmRemoveHiddenTag(
+                      '${book.id}',
+                      '${tag.id}'
+                    )
+                  "
+                >
+                  ${
+                    isRemoveReady
+                      ? `削除？ ${tag.name}`
+                      : tag.name
+                  }
+                </span>
+              `;
+            }).join("")
+          }
+        </div>
 
-  </div>
-  
-      `
+      </div>
+    `
     : ""
 }
-       
-       
-  <input
-  class="input-common input-small"
-  id="hidden-tag-input-${book.id}"
-  type="text"
-  placeholder="管理タグを追加"
-  oninput="renderHiddenTagSuggest('${book.id}')"
-  onkeydown="
-    if(event.key==='Enter'){
-      addHiddenTag('${book.id}', this.value);
-    }
-  "
->
 
+<div class="hidden-tag-input-area">
+  <input
+    class="input-common input-small"
+    id="hidden-tag-input-${book.id}"
+    type="text"
+    placeholder="管理タグを追加"
+    oninput="renderHiddenTagSuggest('${book.id}')"
+    onkeydown="
+      if(event.key==='Enter'){
+        addHiddenTag('${book.id}', this.value);
+      }
+    "
+  >
 </div>
- <div
+
+<div
   id="hidden-tag-suggest-${book.id}"
   class="hidden-tag-suggest"
 ></div>
