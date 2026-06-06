8303a3a8751c1040e9bee431d4fd2d2a16aa5229
sinjouji <mail0of0iris@gmail.com>
2026-06-06 19:11:19 +0900

    Add files via upload

--- js/books.js
+++ js/books.js
@@ -136,6 +136,365 @@ async function removeReadDate(bookId,date){
 }
 
 
+
+
+//==============================
+//新規本を追加保存================
+//=============================
+async function saveNewBook(){
+
+  const title =
+    document.getElementById("add-title").value.trim();
+
+  if(!title){
+    alert("タイトルが未入力です");
+    return;
+  }
+
+  const date =
+    document.getElementById("add-date").value;
+
+  const memo =
+    document.getElementById("add-memo")?.value || "";
+
+
+  const sameTitleBooks =
+  books.filter(book =>
+
+    (book.title || "").trim()
+      === title
+
+  );
+
+const maxVolume =
+  Math.max(
+    0,
+    ...sameTitleBooks.map(book =>
+      Number(book.volume || 0)
+    )
+  );
+
+const nextVolume =
+  maxVolume + 1;
+
+
+  const book = {
+    id: Date.now().toString(),
+    title,
+    volume: nextVolume,
+    memo: memo,
+    fav: newBookFav,
+    readDates: date ? [date] : [],
+    tagIds: [
+      ...newBookTagIds,
+      ...newBookHiddenTagIds
+    ],
+    seriesIds:
+      editingBookSeriesIds.map(String),
+    type: date ? "normal" : "wish"
+  };
+
+  books.unshift(book);
+  
+  
+  seriesMaster.forEach(series=>{
+
+  const shouldHaveSeries =
+    editingBookSeriesIds.includes(
+      String(series.id)
+    );
+
+  if(shouldHaveSeries){
+
+    if(
+      !(series.bookIds || [])
+        .includes(String(book.id))
+    ){
+
+      series.bookIds =
+        (series.bookIds || []).concat(
+          String(book.id)
+        );
+    }
+  }
+});
+  
+
+  await saveData();
+  
+  const placeName =
+    book.type === "wish"
+      ? "ウィッシュリスト"
+      : "本棚";
+      
+  showToast(`「${book.title}」を${placeName}に追加しました`);
+
+  closeModal("add-book-modal");
+
+  renderHome();
+}
+
+
+
+
+//==============================
+//====詳細保存================
+//==============================
+async function saveDetail(id){
+
+ const book =
+    books.find(b=>String(b.id)===String(id));
+
+  console.log("save start", id);
+  
+  if(!book.readDates){
+
+  book.readDates =
+    book.dates || [];
+}
+delete book.dates;
+
+ book.seriesIds =
+  [...editingBookSeriesIds];
+
+seriesMaster.forEach(series=>{
+
+  const hasBook =
+    editingBookSeriesIds.includes(
+      String(series.id)
+    );
+
+  if(hasBook){
+
+    if(
+      !(series.bookIds || [])
+        .includes(book.id)
+    ){
+
+      series.bookIds = [
+        ...(series.bookIds || []),
+        book.id
+      ];
+    }
+
+  }else{
+
+    series.bookIds =
+      (series.bookIds || [])
+        .filter(seriesBookId =>
+
+          String(seriesBookId)
+          !== String(book.id)
+
+        );
+  }
+});
+
+  console.log("found book", book);
+
+  if(!book) return;
+
+  console.log(
+    document.getElementById("detail-title").value
+  );
+
+  book.title =
+    document.getElementById("detail-title").value;
+
+ if(enableMemo){
+   const memoEl =
+     document.getElementById("editMemo");
+     
+     book.memo =
+       memoEl?.value || "";
+  }
+
+  book.fav = currentDetailFav;
+
+  console.log("after edit", book);
+
+  await saveData();
+
+showToast("保存しました！");
+
+  closeModal("open-book-modal");
+
+  renderHome();
+}
+
+
+
+
+//==============================
+//====本を削除==================
+//==============================
+async function deleteBook(id){
+
+  const book =
+    books.find(
+      b => String(b.id) === String(id)
+    );
+
+  if(!book) return;
+
+  const title = book.title;
+
+  const ok =
+    confirm(
+      `${book.title}を削除しますか？`
+    );
+
+  if(!ok) return;
+
+  books =
+    books.filter(
+      b => String(b.id) !== String(id)
+    );
+
+  seriesMaster.forEach(series=>{
+
+    series.bookIds =
+      (series.bookIds || [])
+        .filter(seriesBookId =>
+
+          String(seriesBookId)
+          !== String(book.id)
+
+        );
+  });
+
+  await saveData();
+
+  closeModal("open-book-modal");
+
+  renderHome();
+
+  showToast(
+    `「${title}」を削除しました`
+  );
+}
+
+
+
+
+
+//==============================
+//本詳細の関連本の一覧描画
+//==============================
+function renderBookEditSeries(
+  targetId
+){
+
+  const target =
+    document.getElementById(targetId);
+
+  if(!target) return;
+
+  const relatedSeries =
+    seriesMaster.filter(s =>
+
+      editingBookSeriesIds.includes(
+        String(s.id)
+      )
+
+    );
+
+  target.innerHTML = `
+
+    <div>
+
+      ${relatedSeries.map(s=>`
+
+        <div class="related-chip">
+
+          ${s.name}
+
+          <button
+            class="mini-delete-btn"
+            onclick="
+              removeSeriesFromBook(
+                '${s.id}',
+                '${targetId}',
+                'book-related-search',
+                'book-series-suggest'
+              )
+            "
+          >
+            ✕
+          </button>
+
+        </div>
+
+      `).join("")}
+
+    </div>
+  `;
+}
+
+
+
+
+//==============================
+//本詳細に関連本の追加処理
+//==============================
+function addSeriesToBook(
+  id,
+  listId,
+  searchId,
+  suggestId
+){
+
+  if(
+    editingBookSeriesIds.includes(
+      String(id)
+    )
+  ){
+    return;
+  }
+  editingBookSeriesIds.push(
+    String(id)
+  );
+
+  renderBookEditSeries(listId);
+
+  renderBookSeriesSuggest(
+    searchId,
+    suggestId
+  );
+}
+
+
+//==============================
+//本詳細の関連削除（シリーズ）
+//==============================
+function removeSeriesFromBook(
+  id,
+  searchId,
+  suggestId,
+  listId
+){
+
+if(!confirm("削除しますか？")){
+  return;
+}
+
+  editingBookSeriesIds =
+    editingBookSeriesIds.filter(
+      sId => String(sId) !== String(id)
+    );
+
+  renderBookEditSeries(
+    searchId,
+    suggestId,
+    listId
+    );
+
+}
+
+
+
+
+
 //==============================
 //本の複製
 //==============================
