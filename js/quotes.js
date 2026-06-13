9fce2c977556b3434853dc325ac2bc29b99fbc9f
sinjouji <mail0of0iris@gmail.com>
2026-06-13 21:15:42 +0900

    Add files via upload

--- js/quotes.js
+++ js/quotes.js
@@ -35,60 +35,11 @@ function renderQuotes(){
       </div>
 
       <div id="quote-list-page">
-      
-      document.getElementById(
-  "quote-list-page"
-).innerHTML =
 
-allQuotes.map(item=>`
-
-  <div class="quote-card">
-
-    ${
-      item.quote.favorite
-        ? "⭐"
-        : ""
-    }
-
-    <div class="quote-text">
-
-      ${
-        item.quote.text.length > 60
-
-          ? item.quote.text.slice(0,60) + "..."
-
-          : item.quote.text
-      }
-
-    </div>
-
-    <div class="quote-book">
-
-      ${item.bookTitle}
-      ${item.volume ? ` ${item.volume}巻` : ""}
-
-    </div>
-
-    ${
-      item.quote.memo
-
-        ? `
-          <div class="quote-memo">
-            ${item.quote.memo}
-          </div>
-        `
-
-        : ""
-    }
-
-  </div>
-
-`).join("");
       
       </div>
     `
   });
-
 const allQuotes =
   getAllQuotes();
 
