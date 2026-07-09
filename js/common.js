d270f02ddbc34b87552474f5cd392ac69bb70691
sinjouji <mail0of0iris@gmail.com>
2026-07-09 13:02:46 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -26,6 +26,47 @@ function getVolumeNumber(book){
 
 
 
+//==============================
+// マージ確認用
+//==============================
+function mergeById(currentArray, importedArray){
 
+  const map = new Map();
 
+  currentArray.forEach(item=>{
+    map.set(String(item.id), item);
+  });
+
+  let updated = 0;
+  let added = 0;
+
+  importedArray.forEach(item=>{
+
+    const id = String(item.id);
+
+    if(map.has(id)){
+
+      map.set(id, item);
+      updated++;
+
+    }else{
+
+      map.set(id, item);
+      added++;
+
+    }
+
+  });
+
+  return {
+
+    array: [...map.values()],
+
+    updated,
+
+    added
+
+  };
+
+}
 
